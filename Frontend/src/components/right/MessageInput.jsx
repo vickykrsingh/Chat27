ff49da13ece4a95ec00bf3c24bf022c932560cff
vickykrsingh27 <vickykrsingh27@gmail.com>
2024-12-27 05:22:20 +0530

    integrated socket.io with some bugs while sending message will be fixed soon

--- Frontend/src/components/right/MessageInput.jsx
+++ Frontend/src/components/right/MessageInput.jsx
@@ -2,16 +2,31 @@ import axios from 'axios'
 import React, { useContext, useState } from 'react'
 import toast from 'react-hot-toast'
 import { SelectedChatContext } from '../../context/SelectedChat'
+import { SocketContext } from '../../context/SocketContext'
+import { MessageContext } from '../../context/MessageContext'
 
 function MessageInput() {
-  const [message,setMessage] = useState('')
+  const [inputMessage,setInputMessage] = useState('')
+  const {message,setMessage} = useContext(MessageContext)
   const {selectedId} = useContext(SelectedChatContext)
+  const {socket,setSocket} = useContext(SocketContext)
   const handleSendMessage = async (e) => {
     e.preventDefault()
     try {
-      const {data} = await axios.post(`/message/send/${selectedId}`,{message})
+      const {data} = await axios.post(`/message/send/${selectedId}`,{message:inputMessage})
       if(data.success){
-        setMessage((prev)=>prev='')
+        setInputMessage((prev)=>prev='')
+        await socket.on('newMessage',(data)=>{
+          console.log(data)
+          setMessage([...message,data])
+          console.log(message,'total message')
+        })
+        // socket.on('newMessageSender',(data)=>{
+        //   console.log(data)
+        //   setMessage(prev=>prev=[...message,data])
+        //   console.log(message,'total message')
+        // })
+        return ()=>socket.close()
       }else{
         toast.error("Please try again")
       }
@@ -22,7 +37,7 @@ function MessageInput() {
   return (
     <section>
       <form onSubmit={handleSendMessage} className='flex items-center p-2 bg-slate-700 h-[7vh]'>
-        <input value={message} onChange={(e)=>setMessage(e.target.value)} type='text' placeholder='Type a message' className='flex-1 p-2 bg-slate-700 outline-none text-gray-100'/>
+        <input value={inputMessage} onChange={(e)=>setInputMessage(e.target.value)} type='text' placeholder='Type a message' className='flex-1 p-2 bg-slate-700 outline-none text-gray-100'/>
         <button className='p-2 bg-slate-700 text-gray-100'>Send</button >
       </form>
     </section>
