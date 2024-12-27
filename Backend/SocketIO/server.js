a2710998a6c99fe4e54c031728d7389715f46589
vickykrsingh27 <vickykrsingh27@gmail.com>
2024-12-28 03:44:28 +0530

    changed socket.io cors option

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -7,9 +7,8 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server,{
     cors:{
-        origin:process.env.CLIENT_URL,
+        origin:[process.env.CLIENT_URL,'http://chat027-server.vercel.app'],
         credentials:true,
-        methods:['GET','POST','PUT','DELETE']
     }
 })
 const users = {}
