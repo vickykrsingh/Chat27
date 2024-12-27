5eedbbd330c8b73bd40b29dc34a13ba5e3fd51a9
vickykrsingh27 <vickykrsingh27@gmail.com>
2024-12-28 03:34:13 +0530

    changed cookies option

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server,{
     cors:{
-        origin:"*",
+        origin:process.env.CLIENT_URL,
         credentials:true,
         methods:['GET','POST','PUT','DELETE']
     }
