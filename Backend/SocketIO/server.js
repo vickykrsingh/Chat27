119a5b393a864ad950c524aee30057a801a72526
vickykrsingh27 <vickykrsingh27@gmail.com>
2024-12-27 20:49:49 +0530

    added dotenv configuration in socket io server

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -1,7 +1,8 @@
 import {Server} from 'socket.io'
 import http from 'http'
 import express from 'express'
-
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server,{
