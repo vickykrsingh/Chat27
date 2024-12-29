104f502cc641f2564ff72052995ac3d2132e133d
vickykrsingh27 <vickykrsingh27@gmail.com>
2024-12-29 18:33:36 +0530

    set secure to false

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -29,7 +29,7 @@ export const loginController = async (req,res) => {
         res.cookie('token', token, {
             maxAge: 3600000,
             httpOnly: true,
-            secure:process.env.NODE_ENV==='production'
+            // secure:process.env.NODE_ENV==='production'
         });
         return res.status(200).json({
             success:true,
